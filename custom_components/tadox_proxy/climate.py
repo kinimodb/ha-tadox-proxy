a34912e64b4fac6e0106679a63300217628bd330
kinimodb <buehler.d@outlook.de>
2025-12-21 15:50:58 +0100

    Fix ATTR_TEMPERATURE import and temperature_unit

--- custom_components/tadox_proxy/climate.py
+++ custom_components/tadox_proxy/climate.py
@@ -7,8 +7,8 @@ from homeassistant.components.climate.const import (
     ATTR_HVAC_MODE,
     ATTR_TARGET_TEMP_HIGH,
     ATTR_TARGET_TEMP_LOW,
-    ATTR_TEMPERATURE,
 )
+from homeassistant.const import ATTR_TEMPERATURE
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -25,27 +25,23 @@ async def async_setup_entry(
     """Set up the proxy climate entity."""
     name = entry.data[CONF_NAME]
     source_entity_id = entry.data[CONF_SOURCE_ENTITY_ID]
-    async_add_entities([TadoxProxyClimate(hass, entry.entry_id, name, source_entity_id)])
+    async_add_entities([TadoxProxyClimate(hass, name, source_entity_id)])
 
 
 class TadoxProxyClimate(ClimateEntity):
     """Proxy a source climate entity (forward commands and mirror state)."""
 
-    def __init__(
-        self,
-        hass: HomeAssistant,
-        entry_id: str,
-        name: str,
-        source_entity_id: str,
-    ) -> None:
+    def __init__(self, hass: HomeAssistant, name: str, source_entity_id: str) -> None:
         self.hass = hass
         self._source_entity_id = source_entity_id
         self._remove_listener = None
 
         self._attr_name = name
-        # Stabiler Unique ID: 1 Proxy pro Quell-Entity
         self._attr_unique_id = f"{source_entity_id}_proxy"
         self._attr_should_poll = False
+
+        # WICHTIG: In deiner HA-Version erwartet ClimateEntity dieses Attribut
+        self._attr_temperature_unit = hass.config.units.temperature_unit
 
     def _source_state(self):
         return self.hass.states.get(self._source_entity_id)
@@ -61,127 +57,59 @@ class TadoxProxyClimate(ClimateEntity):
         if st is None:
             return {"source_entity_id": self._source_entity_id}
         attrs = dict(st.attributes)
-        # Quelle als Attribut sichtbar machen (Debug/Transparenz)
         attrs["source_entity_id"] = self._source_entity_id
         return attrs
 
     @property
-    def temperature_unit(self) -> str:
-        return self.hass.config.units.temperature_unit
-
-    @property
     def hvac_mode(self):
         st = self._source_state()
-        if st is None:
-            return None
-        # Bei climate-Entities ist der state typischerweise der hvac_mode ("heat", "off", ...)
-        return st.state
+        return None if st is None else st.state
 
     @property
     def hvac_modes(self):
         st = self._source_state()
-        if st is None:
-            return []
-        return st.attributes.get("hvac_modes", [])
+        return [] if st is None else st.attributes.get("hvac_modes", [])
 
     @property
     def supported_features(self) -> int:
         st = self._source_state()
-        if st is None:
-            return 0
-        return int(st.attributes.get("supported_features", 0))
+        return 0 if st is None else int(st.attributes.get("supported_features", 0))
 
     @property
     def current_temperature(self):
         st = self._source_state()
-        if st is None:
-            return None
-        return st.attributes.get("current_temperature")
+        return None if st is None else st.attributes.get("current_temperature")
 
     @property
     def target_temperature(self):
         st = self._source_state()
-        if st is None:
-            return None
-        return st.attributes.get("temperature")
+        return None if st is None else st.attributes.get("temperature")
 
     @property
     def target_temperature_low(self):
         st = self._source_state()
-        if st is None:
-            return None
-        return st.attributes.get(ATTR_TARGET_TEMP_LOW)
+        return None if st is None else st.attributes.get(ATTR_TARGET_TEMP_LOW)
 
     @property
     def target_temperature_high(self):
         st = self._source_state()
-        if st is None:
-            return None
-        return st.attributes.get(ATTR_TARGET_TEMP_HIGH)
+        return None if st is None else st.attributes.get(ATTR_TARGET_TEMP_HIGH)
 
     @property
     def min_temp(self):
         st = self._source_state()
-        if st is None:
-            return None
-        return st.attributes.get("min_temp")
+        return None if st is None else st.attributes.get("min_temp")
 
     @property
     def max_temp(self):
         st = self._source_state()
-        if st is None:
-            return None
-        return st.attributes.get("max_temp")
+        return None if st is None else st.attributes.get("max_temp")
 
     @property
     def current_humidity(self):
         st = self._source_state()
-        if st is None:
-            return None
-        return st.attributes.get("current_humidity")
+        return None if st is None else st.attributes.get("current_humidity")
 
     async def async_set_temperature(self, **kwargs: Any) -> None:
         """Forward target temperature to the source entity."""
-        # Unterstützt single-setpoint und (falls die Quelle es kann) low/high.
         service_data: dict[str, Any] = {"entity_id": self._source_entity_id}
-
-        if ATTR_TEMPERATURE in kwargs:
-            service_data[ATTR_TEMPERATURE] = kwargs[ATTR_TEMPERATURE]
-        if ATTR_TARGET_TEMP_LOW in kwargs:
-            service_data[ATTR_TARGET_TEMP_LOW] = kwargs[ATTR_TARGET_TEMP_LOW]
-        if ATTR_TARGET_TEMP_HIGH in kwargs:
-            service_data[ATTR_TARGET_TEMP_HIGH] = kwargs[ATTR_TARGET_TEMP_HIGH]
-
-        await self.hass.services.async_call(
-            "climate",
-            "set_temperature",
-            service_data,
-            blocking=True,
-        )
-
-    async def async_set_hvac_mode(self, hvac_mode: str) -> None:
-        """Forward HVAC mode to the source entity."""
-        await self.hass.services.async_call(
-            "climate",
-            "set_hvac_mode",
-            {"entity_id": self._source_entity_id, ATTR_HVAC_MODE: hvac_mode},
-            blocking=True,
-        )
-
-    async def async_added_to_hass(self) -> None:
-        """Register listener to update when source changes."""
-        @callback
-        def _handle_source_change(event) -> None:
-            self.async_write_ha_state()
-
-        self._remove_listener = async_track_state_change_event(
-            self.hass, [self._source_entity_id], _handle_source_change
-        )
-        # initial state push
-        self.async_write_ha_state()
-
-    async def async_will_remove_from_hass(self) -> None:
-        """Cleanup listener."""
-        if self._remove_listener is not None:
-            self._remove_listener()
-            self._remove_listener = None
