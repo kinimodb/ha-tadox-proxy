b3e839fea44d37448a6742dc4541552b41b8b3b1
kinimodb <buehler.d@outlook.de>
2025-12-23 09:42:16 +0100

    chore(const): centralize sensor option keys

--- custom_components/tadox_proxy/const.py
+++ custom_components/tadox_proxy/const.py
@@ -1,4 +1,12 @@
+"""Constants for the Tado X Proxy integration."""
+
 DOMAIN = "tadox_proxy"
 
 CONF_SOURCE_ENTITY_ID = "source_entity_id"
 CONF_NAME = "name"
+
+# Optional/Configurable external inputs (selected via Options Flow)
+CONF_EXTERNAL_TEMPERATURE_ENTITY_ID = "external_temperature_entity_id"
+CONF_EXTERNAL_HUMIDITY_ENTITY_ID = "external_humidity_entity_id"
+CONF_WINDOW_SENSOR_ENTITY_ID = "window_sensor_entity_id"
+CONF_PRESENCE_SENSOR_ENTITY_ID = "presence_sensor_entity_id"
